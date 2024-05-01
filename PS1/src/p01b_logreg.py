810b55ff1729ea6f9e5a1bb7d6a0df76de70d14b
ronchuxia <ron_chuxia@outlook.com>
2024-05-01 20:34:24 +0800

    p01b_logreg

--- PS1/src/p01b_logreg.py
+++ PS1/src/p01b_logreg.py
@@ -1,8 +1,8 @@
 import numpy as np
 import util
+import os
 
 from linear_model import LinearModel
-
 
 def main(train_path, eval_path, pred_path):
     """Problem 1(b): Logistic regression with Newton's Method.
@@ -19,6 +19,13 @@ def main(train_path, eval_path, pred_path):
     clf = LogisticRegression()
     clf.fit(x_train, y_train)
     y_predict = clf.predict(x_eval)
+    np.savetxt(pred_path, y_predict, fmt="%d")
+
+    eval_accuracy = np.sum(y_predict == y_eval) / y_eval.shape[0]
+    print("Eval Accuracy: ", eval_accuracy)
+
+    util.plot(x_train, y_train, clf.theta, "figures/p01b_train.png")
+    util.plot(x_eval, y_eval, clf.theta, "figures/p01b_val.png")
     # *** END CODE HERE ***
 
 
@@ -49,7 +56,7 @@ class LogisticRegression(LinearModel):
                 Outputs. Shape (m,).
 
             """
-            return 1 / (1 + np.exp(-np.dot(x, self.theta)))
+            return 1 / (1 + np.exp(- x @ self.theta))
 
         def H(x):
             """
@@ -60,11 +67,16 @@ class LogisticRegression(LinearModel):
             Returns:
                 Hessian of J(theta). Shape(n, n).
             """
-            return (np.sum(h(x))) / m
+            return (x.T * h(x) * (1 - h(x))) @ x / x.shape[0]
 
+        m, n = x.shape
+        self.theta = np.zeros(n)    # shape (n, )
 
-        self.theta = np.zeros(x.shape[1])
-
+        v = np.ones(self.theta.shape)
+        while (np.linalg.norm(v) > 1e-5) :
+            d_theta = - (1 / m) * np.sum(np.expand_dims(y - h(x), axis=-1) * x, axis=0) 
+            v = np.linalg.inv(H(x)) @ d_theta
+            self.theta -= v
         # *** END CODE HERE ***
 
     def predict(self, x):
@@ -77,4 +89,13 @@ class LogisticRegression(LinearModel):
             Outputs of shape (m,).
         """
         # *** START CODE HERE ***
+        prob = 1 / (1 + np.exp(- x @ self.theta))
+        pred = prob > 0.5
+        return pred
         # *** END CODE HERE ***
+
+if __name__ == "__main__":
+    main(train_path='../data/ds1_train.csv',
+         eval_path='../data/ds1_valid.csv',
+         pred_path='output/p01b_pred_1.txt')
+    
